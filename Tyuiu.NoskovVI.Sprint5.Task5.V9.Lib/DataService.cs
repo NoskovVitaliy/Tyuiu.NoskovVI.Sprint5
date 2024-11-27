02456fa5e1022fb6e903d756667561674d8e1cf7
NoskovVitaliy <Tyuiu.NoskovVI@outlook.com>
2024-11-27 13:52:07 +0500

    Task5

--- Tyuiu.NoskovVI.Sprint5.Task5.V9.Lib/DataService.cs
+++ Tyuiu.NoskovVI.Sprint5.Task5.V9.Lib/DataService.cs
@@ -6,13 +6,11 @@ namespace Tyuiu.NoskovVI.Sprint5.Task5.V9.Lib
         public double LoadFromDataFile(string path)
         {
             int res = 0;
-            using (StreamReader reader = new StreamReader(path))
+            string file = File.ReadAllText(path);
+            foreach (string line in file.Split(" "))
             {
-                while (reader.EndOfStream == false)
-                {
-                    int.TryParse(reader.ReadLine(), out int number);
-                    if (number > res) res = number;
-                }
+                int.TryParse(line, out int number);
+                if (number > res) res = number;
             }
             return res;
         }
